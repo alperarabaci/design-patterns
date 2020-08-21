fd17d6eee9a99c9fb4a9630577be7d1ee26d1f48
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-08-21 14:58:36 +0300

    5. "Open-Closed Principle - Hands On" end

--- s1_open_closed_principle/src/main/java/com/company/openclose/ISPSubscriber.java
+++ s1_open_closed_principle/src/main/java/com/company/openclose/ISPSubscriber.java
@@ -2,15 +2,8 @@ package com.company.openclose;
 
 import java.util.List;
 
-public class ISPSubscriber {
+public class ISPSubscriber extends Subscriber {
 
-    private Long subscriberId;
-
-    private String address;
-
-    private Long phoneNumber;
-
-    private int baseRate;
 
     private long freeUsage;
 
@@ -18,68 +11,14 @@ public class ISPSubscriber {
 
     }
 
+    @Override
     public double calculateBill() {
         List<InternetSessionHistory.InternetSession> sessions = InternetSessionHistory.getCurrentSessions(subscriberId);
         long totalData = sessions.stream().mapToLong(InternetSessionHistory.InternetSession::getDataUsed).sum();
         long chargeableData = totalData - freeUsage;
-        return chargeableData*baseRate/100;
+        return chargeableData* baseRate/100;
     }
 
-    /**
-     * @return the subscriberId
-     */
-    public Long getSubscriberId() {
-        return subscriberId;
-    }
-
-    /**
-     * @param subscriberId the subscriberId to set
-     */
-    public void setSubscriberId(Long subscriberId) {
-        this.subscriberId = subscriberId;
-    }
-
-    /**
-     * @return the address
-     */
-    public String getAddress() {
-        return address;
-    }
-
-    /**
-     * @param address the address to set
-     */
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    /**
-     * @return the phoneNumber
-     */
-    public Long getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    /**
-     * @param phoneNumber the phoneNumber to set
-     */
-    public void setPhoneNumber(Long phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    /**
-     * @return the baseRate
-     */
-    public int getBaseRate() {
-        return baseRate;
-    }
-
-    /**
-     * @param baseRate the baseRate to set
-     */
-    public void setBaseRate(int baseRate) {
-        this.baseRate = baseRate;
-    }
 
     /**
      * @return the freeUsage
--- s1_open_closed_principle/src/main/java/com/company/openclose/Subscriber.java
+++ s1_open_closed_principle/src/main/java/com/company/openclose/Subscriber.java
@@ -1,6 +1,72 @@
 package com.company.openclose;
 
 
-public class Subscriber {
+public abstract class Subscriber {
+
+	protected Long subscriberId;
+
+    protected String address;
+
+    protected Long phoneNumber;
+
+    protected int baseRate;
+    
+    public abstract double calculateBill();
+    
+    /**
+     * @return the subscriberId
+     */
+    public Long getSubscriberId() {
+        return subscriberId;
+    }
+
+    /**
+     * @param subscriberId the subscriberId to set
+     */
+    public void setSubscriberId(Long subscriberId) {
+        this.subscriberId = subscriberId;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return the phoneNumber
+     */
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     * @param phoneNumber the phoneNumber to set
+     */
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    /**
+     * @return the baseRate
+     */
+    public int getBaseRate() {
+        return baseRate;
+    }
+
+    /**
+     * @param baseRate the baseRate to set
+     */
+    public void setBaseRate(int baseRate) {
+        this.baseRate = baseRate;
+    }
 
 }
