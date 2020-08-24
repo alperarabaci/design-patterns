eb7ea4a2437208f931d354aa10790d692adc9298
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-08-24 15:50:03 +0300

    93. "Bridge - Implementation & Design Considerations" end

--- s13-bridge/src/main/java/com/coffeepoweredcrew/bridge/Queue.java
+++ s13-bridge/src/main/java/com/coffeepoweredcrew/bridge/Queue.java
@@ -1,7 +1,22 @@
 package com.coffeepoweredcrew.bridge;
 
 //A refined abstraction.
-public class Queue<T> {
+public class Queue<T> implements FifoCollection<T>{
+
+	private LinkedList<T> list;
 	
+	public Queue(LinkedList<T> list) {
+		this.list = list;
+	}
+	
+	@Override
+	public void offer(T element) {
+		list.addLast(element);
+	}
+
+	@Override
+	public T poll() {
+		return list.removeFirst();
+	}
 	
 }
