8e5d95c4b474223998916a197cd70edb09f2759a
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-08-24 19:43:28 +0300

    Section 17: "Flyweight" begin

---
+++ s17-flyweight/src/main/java/com/coffeepoweredcrew/flyweight/Client.java
@@ -0,0 +1,11 @@
+package com.coffeepoweredcrew.flyweight;
+
+import com.coffeepoweredcrew.flyweight.ErrorMessageFactory.ErrorType;
+
+public class Client {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ s17-flyweight/src/main/java/com/coffeepoweredcrew/flyweight/SystemErrorMessage.java
@@ -0,0 +1,7 @@
+package com.coffeepoweredcrew.flyweight;
+
+//A concrete Flyweight. Instance is shared
+public class SystemErrorMessage {
+
+
+}
