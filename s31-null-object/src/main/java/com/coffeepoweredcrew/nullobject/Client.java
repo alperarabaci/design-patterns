98f607e7bda46caea1b2d9a4c333f3c857959055
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-08-27 19:38:25 +0300

    247. Null Object - Implementation

--- s31-null-object/src/main/java/com/coffeepoweredcrew/nullobject/Client.java
+++ s31-null-object/src/main/java/com/coffeepoweredcrew/nullobject/Client.java
@@ -5,7 +5,9 @@ public class Client {
 	public static void main(String[] args) {
 		ComplexService service = new ComplexService("Simple report",new StorageService());
 		service.generateReport();
-		
+
+		ComplexService service2 = new ComplexService("Simple report",new NullStorageService());
+		service2.generateReport();
 	}
 
 }
--- s31-null-object/src/main/java/com/coffeepoweredcrew/nullobject/NullStorageService.java
+++ s31-null-object/src/main/java/com/coffeepoweredcrew/nullobject/NullStorageService.java
@@ -1,5 +1,9 @@
 package com.coffeepoweredcrew.nullobject;
 
-public class NullStorageService{
+public class NullStorageService extends StorageService{
 
+	@Override
+	public void save(Report report) {
+		System.out.println("Null object save method. Doing nothing.");
+	}
 }
